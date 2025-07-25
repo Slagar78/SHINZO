0c87d8f1800813731543efd040c7bcc0f88fa5e9
Slagar78 <rolemon95@gmail.com>
2025-07-25 16:14:10 +0400

    SHINZO
    Кледон сити -обновление, добавлены деревья на карту, заменены тайлы земли

--- disasm/data/maps/entries/map01/2-areas.asm
+++ disasm/data/maps/entries/map01/2-areas.asm
@@ -3,14 +3,13 @@
 ; 0x95ECA..0x95F26 : 
                 
                     mainLayerStart      0, 0
-                    mainLayerEnd        58, 31
-                    scndLayerFgndStart  0, 0
+                    mainLayerEnd        57, 31
+                    scndLayerFgndStart  0, 32
                     scndLayerBgndStart  0, 0
                     mainLayerParallax   256, 256
                     scndLayerParallax   256, 256
                     mainLayerAutoscroll 0, 0
                     scndLayerAutoscroll 0, 0
-                    mainLayerType    255
+                    mainLayerType    0
                     areaDefaultMusic 35
-
                 endWord
