7f62bccb1b443529b9d43f4bb197560d121c9c9e
Slagar78 <rolemon95@gmail.com>
2025-08-18 16:26:23 +0300

    SHINZO
    MAP 1

--- disasm/data/maps/entries/map01/00-tilesets.asm
+++ disasm/data/maps/entries/map01/00-tilesets.asm
@@ -1,9 +1,9 @@
 
-; ASM FILE data\maps\entries\map01\00-tilesets.asm :
-; 0x95E9C..0x95EA2 : 
-                mapPalette  1
-                mapTileset1 1
-                mapTileset2 2
-                mapTileset3 3
-                mapTileset4 4
-                mapTileset5 5
+; ASM FILE data\maps\entries\map01\00-tilesets.asm : 
+
+				mapPalette  1
+				mapTileset1 1
+				mapTileset2 2
+				mapTileset3 3
+				mapTileset4 4
+				mapTileset5 5
